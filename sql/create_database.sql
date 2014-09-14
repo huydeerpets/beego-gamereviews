b0aa12f062e85450233ba9f40d713e4795ae11a4
Makoto Kudo <dev@mof-brown.jp>
2014-09-14 22:05:47 +0900

    modified grant sql.

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,3 +1,4 @@
 create database gamereviews default character set utf8;
 
 grant all privileges on gamereviews.* to 'gamereviews'@'%' identified by 'gamereviews';
+grant all privileges on gamereviews.* to 'gamereviews'@'localhost' identified by 'gamereviews';
